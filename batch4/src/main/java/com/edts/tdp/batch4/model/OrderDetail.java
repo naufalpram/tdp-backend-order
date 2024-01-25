555af07f685406abef920e7957965230f3d23bee
Naufal Pramudya Yusuf <naufal.pramudya@sg-dsa.com>
2024-01-25 15:01:37 +0700

    feat: add columns in OrderDetail model

--- batch4/src/main/java/com/edts/tdp/batch4/model/OrderDetail.java
+++ batch4/src/main/java/com/edts/tdp/batch4/model/OrderDetail.java
@@ -1,9 +1,46 @@
 package com.edts.tdp.batch4.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "order_detail")
 public class OrderDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "created_by", nullable = false)
+    private String createdBy;
+
+    @Column(name = "modified_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime modifiedAt;
+
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "order_header_id", referencedColumnName = "id")
+    private OrderHeader orderHeader;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column
+    private int qty;
+
+    @Column
+    private double price;
+
+    public OrderDetail() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
