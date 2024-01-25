eb517c2042f2359be0afc44e1fe12190c770f2c0
Amini Dwi Puspita <amini.dwi@sg-dsa.com>
2024-01-25 16:47:50 +0700

    add comment and relasi database

--- batch4/src/main/java/com/edts/tdp/batch4/model/OrderHeader.java
+++ batch4/src/main/java/com/edts/tdp/batch4/model/OrderHeader.java
@@ -1,9 +1,12 @@
 package com.edts.tdp.batch4.model;
 
 import com.edts.tdp.batch4.constant.Status;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 @Entity
@@ -40,6 +43,27 @@ public class OrderHeader {
 
     @Column(name = "status", nullable = false)
     private String status;
+
+    /*
+     *@JsonManagedReference digunakan untuk mereference  hubungan
+     *@OneToMany Hubungan ini menggambarkan relasi tabel dari order_header dengan orderDetail
+     *mappedBy digunakan untuk menjelaskan tabel mana yang akan dihubungkan
+     *list digunakan karena header bisa punya banyak produk, sedangkan list hanya punya saru makanya masuk ke orderDetail
+     *private OrderDelivery orderDelivery; ngreference ke classnya
+     */
+    @JsonManagedReference
+    @OneToMany(mappedBy = "order_header")
+    private List<OrderDetail> orderDetailList;
+
+    /*
+    *@OneToOne Hubungan ini menggambarkan relasi tabel dari order_header dengan orderDelivery
+    *mappedBy digunakan untuk menjelaskan tabel mana yang akan dihubungkan
+    *@JsonIgnoreProperties ini untuk mencegah pembacaan berulang dari relasi
+    *private OrderDelivery orderDelivery; ngreference ke classnya
+    */
+    @OneToOne(mappedBy = "order_header")
+    @JsonIgnoreProperties({"order_header"})
+    private OrderDelivery orderDelivery;
 
     public OrderHeader() {
         this.createdAt = LocalDateTime.now();
