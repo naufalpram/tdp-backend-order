9e56331400b44ae1afebe38441a2efcad07d7133
Naufal Pramudya Yusuf <naufal.pramudya@sg-dsa.com>
2024-01-25 11:01:15 +0700

    Initial commit

---
+++ batch4/src/test/java/com/edts/tdp/batch4/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edts.tdp.batch4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
