d74f84a18ffc764f694520c3cac6c737b0fcebd1
Irfanul Arifa <irfanul.arifa@sg-dsa.com>
2024-01-26 17:43:42 +0700

    feat : Edit Order Service

--- batch4/src/main/java/com/edts/tdp/batch4/controller/OrderController.java
+++ batch4/src/main/java/com/edts/tdp/batch4/controller/OrderController.java
@@ -1,10 +1,18 @@
 package com.edts.tdp.batch4.controller;
 
+import com.edts.tdp.batch4.service.OrderLogicService;
+import com.edts.tdp.batch4.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api/v1/order")
 public class OrderController {
+    @Autowired
+    private OrderService orderService;
+    private OrderLogicService orderLogicService;
 
 }
--- batch4/src/main/java/com/edts/tdp/batch4/service/OrderService.java
+++ batch4/src/main/java/com/edts/tdp/batch4/service/OrderService.java
@@ -1,18 +1,30 @@
 package com.edts.tdp.batch4.service;
 
 import com.edts.tdp.batch4.model.OrderHeader;
+import com.edts.tdp.batch4.repository.OrderDeliveryRepository;
+import com.edts.tdp.batch4.repository.OrderDetailRepository;
 import com.edts.tdp.batch4.repository.OrderHeaderRepository;
+import org.hibernate.query.Order;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class OrderService {
-
+public class OrderService{
     @Autowired
     private OrderHeaderRepository orderHeaderRepository;
-//    public List<?> fetchListOrderByStatus(String status) {
-////        List<OrderHeader> orderHeadersList =
-//    }
+    private OrderDetailRepository orderDetailRepository;
+    private OrderDeliveryRepository orderDeliveryRepository;
+
+    public Page<OrderHeader> findAllByCustomerIdAndStatus(long customerId,
+                                                          String status,
+                                                          int pageNumber,
+                                                          int size) {
+        Pageable pageable = PageRequest.of(pageNumber,size);
+        return orderHeaderRepository.findAllByCustomerIdAndStatus(customerId,status,pageable);
+    }
 }
