42b81bd0f8379381202715dd46e2d13085b673d2
Naufal Pramudya Yusuf <naufal.pramudya@sg-dsa.com>
2024-01-26 15:52:35 +0700

    feat: add findAllByCustomerId for order header repo

--- batch4/src/main/java/com/edts/tdp/batch4/repository/OrderHeaderRepository.java
+++ batch4/src/main/java/com/edts/tdp/batch4/repository/OrderHeaderRepository.java
@@ -1,9 +1,12 @@
 package com.edts.tdp.batch4.repository;
 
 import com.edts.tdp.batch4.model.OrderHeader;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderHeaderRepository extends JpaRepository<OrderHeader, Integer> {
+    Page<OrderHeader> findAllByCustomerId(Long customerId, Pageable pageable);
 }
