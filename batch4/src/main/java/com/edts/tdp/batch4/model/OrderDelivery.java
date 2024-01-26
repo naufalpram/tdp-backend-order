0d2eb25d9d87d048cb831c5e2fbb902f45a8e338
Naufal Pramudya Yusuf <naufal.pramudya@sg-dsa.com>
2024-01-26 16:31:13 +0700

    feat: change createdBy and modifiedBy type to string and add length for them

--- batch4/src/main/java/com/edts/tdp/batch4/model/OrderDelivery.java
+++ batch4/src/main/java/com/edts/tdp/batch4/model/OrderDelivery.java
@@ -10,21 +10,42 @@ import java.time.LocalDateTime;
 public class OrderDelivery {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column private long id;
-    @Column(name = "created_at", columnDefinition = "TIMESTAMP") private LocalDateTime createdAt;
-    @Column(name = "created_by", nullable = false) private String createdBy;
-    @Column(name = "modified_at", columnDefinition = "TIMESTAMP") private LocalDateTime modifiedAt;
-    @Column(name = "modified_by") private String modifiedBy;
+    @Column
+    private long id;
+
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @Column(name = "created_by", nullable = false, length = 20)
+    private String createdBy;
+
+    @Column(name = "modified_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime modifiedAt;
+
+    @Column(name = "modified_by", length = 20)
+    private String modifiedBy;
+
     @OneToOne
     @JoinColumn(name = "order_header_id", referencedColumnName = "id")
     @JsonIgnoreProperties({"orderDelivery"})
     private OrderHeader orderHeader;
-    @Column(nullable = false) private String street;
-    @Column(nullable = false) private String province;
-    @Column(name = "distance_in_km", nullable = false) private double distanceInKm;
-    @Column(name = "is_delivered", nullable = false) private boolean isDelivered;
-    @Column(nullable = false) private double latitude;
-    @Column(nullable = false) private double longitude;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String province;
+    @Column(name = "distance_in_km", nullable = false)
+    private double distanceInKm;
+
+    @Column(name = "is_delivered", nullable = false)
+    private boolean isDelivered;
+
+    @Column(nullable = false)
+    private double latitude;
+
+    @Column(nullable = false)
+    private double longitude;
 
     public OrderDelivery() {
         this.createdAt = LocalDateTime.now();
