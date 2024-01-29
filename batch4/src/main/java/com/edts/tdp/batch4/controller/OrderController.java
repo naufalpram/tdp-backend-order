b72946d0429e6c723cb8565880c3c8bb42efa0c8
Irfanul Arifa <irfanul.arifa@sg-dsa.com>
2024-01-29 15:05:15 +0700

    feat : Edit Order Service and Add Endpoint "/update/cancel"

--- batch4/src/main/java/com/edts/tdp/batch4/controller/OrderController.java
+++ batch4/src/main/java/com/edts/tdp/batch4/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.edts.tdp.batch4.controller;
 
 import com.edts.tdp.batch4.bean.BaseResponseBean;
+import com.edts.tdp.batch4.bean.response.CreatedOrderBean;
 import com.edts.tdp.batch4.model.OrderHeader;
 import com.edts.tdp.batch4.service.OrderLogicService;
 import com.edts.tdp.batch4.service.OrderService;
@@ -8,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/order")
@@ -38,5 +36,21 @@ public class OrderController {
         BaseResponseBean<Page<OrderHeader>> responseBean;
         responseBean = orderService.findAllByCustomerIdAndStatus(customerId, status, page, size);
         return new ResponseEntity<>(responseBean, HttpStatus.OK);
+    }
+
+    @PostMapping("/update/sent")
+    public ResponseEntity<BaseResponseBean<CreatedOrderBean>> updateOrderSent(@RequestParam long customerId,
+                                                                              @RequestParam String orderNumber) {
+        BaseResponseBean<CreatedOrderBean> orderBean;
+        orderBean = orderService.updateHistoryStatus(customerId, orderNumber);
+        return new ResponseEntity<>(orderBean, HttpStatus.OK);
+    }
+
+    @PostMapping("update/cancel")
+    public ResponseEntity<BaseResponseBean<CreatedOrderBean>> cancelOrder(@RequestParam long customerId,
+                                                                          @RequestParam String orderNumber) {
+        BaseResponseBean<CreatedOrderBean> orderBean;
+        orderBean = orderService.cancelOrder(customerId, orderNumber);
+        return new ResponseEntity<>(orderBean, HttpStatus.OK);
     }
 }
--- batch4/src/main/java/com/edts/tdp/batch4/repository/OrderHeaderRepository.java
+++ batch4/src/main/java/com/edts/tdp/batch4/repository/OrderHeaderRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderHeaderRepository extends JpaRepository<OrderHeader, Integer> {
     Page<OrderHeader> findAllByCustomerId(Long customerId, Pageable pageable);
     Page<OrderHeader> findAllByCustomerIdAndStatus(long customerId, String status, Pageable pageable);
+    Optional<OrderHeader> findByCustomerIdAndOrderNumber(long customerId, String orderNumber);
 }
