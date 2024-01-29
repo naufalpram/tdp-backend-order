32e7364a7371a52910c7466ead78e0478670d5a7
Amini Dwi Puspita <amini.dwi@sg-dsa.com>
2024-01-29 15:18:35 +0700

    fix: remove double scientific notation on response

--- batch4/src/main/java/com/edts/tdp/batch4/bean/response/CreatedOrderBean.java
+++ batch4/src/main/java/com/edts/tdp/batch4/bean/response/CreatedOrderBean.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 public class CreatedOrderBean {
     private LocalDateTime createdAt;
     private String orderNumber;
-    private double totalPaid;
+    private String totalPaid;
     private String status;
 
 }
